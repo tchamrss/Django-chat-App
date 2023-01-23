2e9e1d2b36bd1ca1f4db84296a663e0d309b1830
Russell <russell.tchamba@gmail.com>
2023-01-23 17:05:56 +0100

    add login

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Chat, Message
+
+class MessageAdmin(admin.ModelAdmin):
+    fields = ('chat','text','created_at', 'author', 'receiver')
+    list_display = ('chat','text','created_at', 'author', 'receiver')
+    search_fields = ('text',)
+    
 
 # Register your models here.
+admin.site.register(Message,MessageAdmin)
+admin.site.register( Chat)
--- chat/views.py
+++ chat/views.py
@@ -1,4 +1,27 @@
-from django.shortcuts import render
 
+from django.shortcuts import render
+from django.http.response import HttpResponseRedirect
+from .models import Chat, Message
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+
+@login_required(login_url='/login/')
 def index(request):
-    return render(request, 'chat/index.html')
+    if request.method == 'POST':
+        print("Received data " + request.POST['textmessage'])
+        myChat = Chat.objects.get(id=1)
+        Message.objects.create(text=request.POST['textmessage'], chat=None, author=request.user, receiver=request.user)
+    chatMessages = Message.objects.filter(chat__id=1)
+    return render(request, 'chat/index.html', {'messages': chatMessages})
+
+
+def login_view(request):
+    redirect = request.GET.get('next')
+    if request.method == 'POST':
+        user = authenticate(username = request.POST.get('username'), password=request.POST.get('password'))
+        if user:
+            login(request, user)
+            return HttpResponseRedirect(request.POST.get('redirect'))
+        else:
+            return render(request,'auth/login.html', { 'wrongPassword': True, 'redirect': redirect})
+    return render(request,'auth/login.html', { 'redirect': redirect })
